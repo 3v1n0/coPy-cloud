007c2fa35b6b89031d6763c4560932803e9107f1
Marco Trevisan (Treviño) <mail@3v1n0.net>
2014-06-12 18:58:34 +0200

    Add GPLv3 copyright statement

--- CoPyCloud.py
+++ CoPyCloud.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2014 Marco Trevisan
+#
+# CoPyCloud: a simple python wrapper to manage the Copy.com cloud
+#
+# Authors:
+#  Marco Trevisan (Treviño) <mail@3v1n0.net>
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; version 3.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUTa
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 import os
 import sys
