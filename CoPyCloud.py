cd127869fe99e05cc6677c30a3315955988d4788
Marco Trevisan (Treviño) <mail@3v1n0.net>
2014-06-16 17:23:48 +0200

    Try to open a file instance in Upload as source file

--- CoPyCloud.py
+++ CoPyCloud.py
@@ -265,10 +265,13 @@ class CoPyCloud:
         return self.__post_req('download_object', {'path': path})
 
     def upload(self, source, dest, parallel=5, share_id=0):
-        try:
-            f = open(source, 'rb')
-        except Exception as e:
-            raise CoPyCloud.Error("Impossible to open source file "+ str(e))
+        if isinstance(source, file):
+            f = source
+        else:
+            try:
+                f = open(source, 'rb')
+            except Exception as e:
+                raise CoPyCloud.Error("Impossible to open source file "+ str(e))
 
         parts = self.__get_file_parts(f)
         parts_chunks = [parts[i:i+parallel] for i in range(0, len(parts), parallel)]
