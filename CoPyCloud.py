773aa81a9140b56577212985f97b19cbdf68fed8
Marco Trevisan (Treviño) <mail@3v1n0.net>
2014-06-16 16:11:52 +0200

    Use better import list

--- CoPyCloud.py
+++ CoPyCloud.py
@@ -20,11 +20,11 @@
 # this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-import os
-import sys
-import json
 import hashlib
+import json
+import os.path
 import struct
+import sys
 import urllib3
 
 class CoPyCloud:
